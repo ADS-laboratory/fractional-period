8533e9940684f3507e769467f40e2e7e3525c516
Leonardo Danelutti <leodane2002@gmail.com>
2023-04-06 10:34:10 +0200

    Updated to new version of lib.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
 use fractional_period::algorithms::{period_naive1, period_naive2, period_smart};
-use fractional_period::random::{StringGen, StringGenFunction, String};
+use fractional_period::random::{String, StringGen, StringGenFunction};
 
 use time_complexity_plot::{
-    distribution::{Distribution, EXPONENTIAL},
-    input::InputBuilder,
+    input::{
+        distribution::{DistributionBuilder, EXPONENTIAL},
+        InputBuilder,
+    },
     measurements::measure,
     plot::time_plot,
 };
 
 fn main() {
-    let length_distribution = Distribution::new(EXPONENTIAL, 1000, 500_000);
+    let length_distribution = DistributionBuilder::new(EXPONENTIAL, 1000, 500_000);
 
     let string_gen = StringGen::new(StringGenFunction::CreateRandomString1, vec![b'a', b'b']);
 
     let string_builder = InputBuilder::new(length_distribution, string_gen);
 
-    let strings = string_builder.generate_inputs_with_repetitions(200, 1);
+    let strings = string_builder.build_with_repetitions(200, 1);
 
     let algorithms: Vec<fn(String) -> usize> = vec![period_naive1, period_naive2, period_smart];
 
