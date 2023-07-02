7adceeee4886a74279672d61cfe1166b69d26e2e
Leonardo Danelutti <leodane2002@gmail.com>
2023-07-02 13:48:56 +0200

    Fixed generation 3

--- src/input.rs
+++ src/input.rs
@@ -92,14 +92,14 @@ fn create_random_string3(n: usize, char_set: &Vec<u8>) -> Vec<u8> {
     let mut s: Vec<u8> = Vec::with_capacity(n);
     let number_of_chars = char_set.len();
     let q = thread_rng().gen_range(1..n);
-    for _ in 0..q - 1 {
+    for _ in 0..q {
         // generate random character
         let char_index = thread_rng().gen_range(0..number_of_chars);
         let char = char_set[char_index];
         s.push(char);
     }
     s.push(new_char(char_set));
-    for i in q..n {
+    for i in q+1..n {
         let char = s[i % q];
         s.push(char);
     }
